9fa5a08d45be7f4810729168ee110d7b74ddf3ac
Qirhi <mochi@Rhiannons-iMac.local>
2021-07-23 09:43:36 -0700

    Item migration, seed complete.  Table generating and seeding properly.  Differentiated user versus item migration.  Complete item model and router.  Endpoints tests.  Will now test to deploy on Heroku

---
+++ api/items/items-model.js
@@ -0,0 +1,59 @@
+// const knex = require('knex');
+// const config = require('../../knexfile.js');
+// const db = knex(config.development);
+
+const db = require("../../data/db-config")
+
+module.exports = {
+  find,
+  findById,
+//   findBy,
+  add,
+  remove,
+  update,
+  findItems,
+};
+
+function find(){
+    console.log("in the find")
+	return db("items")
+		.select("*")
+}
+
+function findById(id) {
+    console.log("in the findById")
+  return db('items')
+    .where({ id })
+    .first();
+}
+
+// function findBy(filter){
+// 	console.log(filter)
+// 	return db("users")
+// 		.select("*")
+// 		.where(filter)
+// }
+
+async function add(item) {
+  const [id] = await db('items').insert(item);
+  return findById(id);
+}
+
+function remove(id) {
+  return db('items')
+    .where({ id })
+    .del();
+}
+
+function update(id, changes) {
+  return db('items')
+    .where({ id })
+    .update(changes, '*');
+}
+
+function findItems(userId) { // HELP
+  return db('items as i')
+    .join( 'users as u', 'u.id', 'i.user_id')
+    .select('i.id', 'i.item_name', 'i.quantity', 'i.price', 'i.description', 'u.id as user_id', 'u.username as user_name')
+    .where({ 'i.user_id': userId });
+}
--- api/server.js
+++ api/server.js
@@ -3,12 +3,14 @@ const cors = require("cors")
 const helmet = require("helmet")
 
 const authRouter = require('./auth/auth-router')
+const itemsRouter = require('./items/items-router')
 
 const server = express()
 
-server.use(helmet())
 server.use(express.json())
+server.use(helmet())
 server.use('/api/auth', authRouter)
+server.use('/api/items', itemsRouter);
 
 server.get("/",(req, res) =>{
     res.json({yodaSays:"Server up it is"})
