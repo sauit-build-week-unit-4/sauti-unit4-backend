927c3e3514087b223a9b9b522fef142d2cf4ec7c
artofmayhem <chef_tony_miller@yahoo.com>
2021-07-22 22:01:49 -1000

    all login and register endpoints and logic 200

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,56 @@
-// Update with your config settings.
-require('dotenv').config();
+// // Update with your config settings.
+// require('dotenv').config();
+// if (process.env.DATABASE_URL) {
+//   pg.defaults.ssl = { rejectUnauthorized: false }
+// }
+
+// const sharedConfig = {
+//   client: 'pg',
+  
+//   migrations: { directory: './data/migrations' },
+//   seeds: { directory: './data/seeds' },
+// }
+
+// module.exports = {
+//   development: {
+//       client: 'sqlite3',
+//       migrations: { directory: './data/migrations' },
+//       seeds: { directory: './data/seeds' },
+//       connection: {
+//         filename: "./data/development.db3"
+//       },
+//       useNullAsDefault: true,
+//       pool: {
+//           afterCreate: (conn, done) => {
+//             conn.run("PRAGMA foreign_keys=ON", done);
+//           },
+//         },
+//   },
+//   testing: {
+//     ...sharedConfig,
+//     connection: process.env.TESTING_DATABASE_URL,
+//   },
+//   production: {
+//     ...sharedConfig,
+//     connection: process.env.DATABASE_URL,
+//     pool: { min: 2, max: 10 },
+//   },
+// }
+require('dotenv').config()
+/*
+  PORT=5000
+  NODE_ENV=development
+  DEV_DATABASE_URL='postgresql://postgres:password@localhost:5432/database_name'
+  TESTING_DATABASE_URL='postgresql://postgres:password@localhost:5432/testing_database_name'
+  Put the above in your .env file. Some adjustments in the connection URLs will be needed:
+    - 5432 (this is the default TCP port for PostgreSQL, should work as is and can be omitted)
+    - postgres (in postgres:password, this is the default superadmin user, might work as is)
+    - password (in postgres:password, replace wnpith the actual password set in pgAdmin 4)
+    - database_name (use the real name of the development database you created in pgAdmin 4)
+    - testing_database_name (use the real name of the testing database you created in pgAdmin 4)
+*/
+const pg = require('pg')
+
 if (process.env.DATABASE_URL) {
   pg.defaults.ssl = { rejectUnauthorized: false }
 }
@@ -14,17 +65,18 @@ const sharedConfig = {
 module.exports = {
   development: {
       client: 'sqlite3',
-      migrations: { directory: './data/migrations' },
-      seeds: { directory: './data/seeds' },
-      connection: {
-        filename: "./data/development.db3"
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+  connection: {
+    filename: "./data/development.db3"
+  },
+  useNullAsDefault: true,
+  pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys=ON", done);
       },
-      useNullAsDefault: true,
-      pool: {
-          afterCreate: (conn, done) => {
-            conn.run("PRAGMA foreign_keys=ON", done);
-          },
-        },
+    },
+
   },
   testing: {
     ...sharedConfig,
