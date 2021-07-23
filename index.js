3132e90f5edf06db4937f5f854daac66dd80c20d
Qirhi <mochi@Rhiannons-iMac.local>
2021-07-23 10:22:21 -0700

    Change Port env var to PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server')
 
-const port = process.env.Port || 5000
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
     console.log(`Listening for the force on port: ${port}`)
