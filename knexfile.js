affd93e86c1de3093a95a3fc3e7b5499395a752b
Qirhi <mochi@Rhiannons-iMac.local>
2021-07-23 10:27:35 -0700

    Update knexfile for prod

--- knexfile.js
+++ knexfile.js
@@ -85,6 +85,29 @@ module.exports = {
   production: {
     ...sharedConfig,
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
     pool: { min: 2, max: 10 },
   },
 }
+
+// production: {
+//   client: "pg",
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+//   pool: {
+//     min: 2,
+//     max: 10,
+//   },
+//   migrations: {
+//     directory: "./data/migrations",
+//   },
+//   seeds: {
+//     directory: "./data/seeds",
+//   },
+// },
