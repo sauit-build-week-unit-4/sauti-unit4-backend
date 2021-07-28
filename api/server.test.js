221dc65775d83df2decc9c60b4adc0d6cdf0fd53
miketillman95 <matillman95@gmail.com>
2021-07-27 19:23:30 -0700

    server test complete, installed supertest, but cant get it to run

---
+++ api/server.test.js
@@ -0,0 +1,18 @@
+const server = require('./server')
+const request = require('supertest')
+
+
+describe('GET /', () => {
+    it('has process.envNODE_ENV as "development" ', () => {
+        expect(process.env.NODE_ENV).toBe('development')
+    })
+
+    it('returns server is up', () => {
+        return request(server).get('/')
+        .expect('content-type', /json/)
+        .expect('content-length', '')
+        .then(res => {
+            expect(res.body.yodaSays).toBe('Server up it is')
+        })
+    })
+})
